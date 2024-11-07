4b7e624b47d0b0497491962ad6e25f9f3dd24562
Abdulkadir Can Kinsiz <kadircankinsiz@gmail.com>
2024-11-07 23:44:32 +0300

    Added for loop for all time data
    Every year, track, race and pilot

--- index.js
+++ index.js
@@ -1,52 +1,135 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+    const scriptContent = fs.readFileSync('tooltip.js', 'utf8');
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [`--window-size=1920,1080`],
+        defaultViewport: {
+            width: 1920,
+            height: 1080
+        }
+    });
 
-    // Web sayfasına gidin
-    await page.goto('https://f1-tempo.com');
+    for (let _i = 0; _i < 7; _i++) {
+        for (let _j = 0; _j < 22; _j++) {
+            for (let _k = 0; _k < 5; _k++) {
+                for (let _p = 0; _p < 20; _p++) {
+                    const page = await browser.newPage();
+                    page.on('console', msg => {
+                        // Log each argument from the console
+                        msg.args().forEach(async (arg) => {
+                            const value = await arg.jsonValue();
+                            //console.log(value);
+                            try {
+                                fs.appendFile('output.txt', value, (err) => {
+                                    if (err) {
+                                        console.error('Error writing to file:', err);
+                                    } else {
+                                        //console.log('File has been written');
+                                    }
+                                });
+                            }catch{}
+                        });
+                    });
+                    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+                    let sleepms = 300;
 
-    // Seçim işlemi için fonksiyon
-    async function selectOption(page, xpath, indexToSelect, pilot) {
-        try {
-            await page.click('xpath/' + xpath);
-            if (pilot) {
-                for (let i = 0; i < indexToSelect; i++) {
-                    await console.log("Arrow");
-                    await page.keyboard.press('ArrowDown'); // Aşağı ok tuşuna bas
-                    await sleep(200);
+                    await page.goto('https://f1-tempo.com');
+
+                    async function selectOption(page, xpath, indexToSelect, pilot) {
+                        try {
+                            await page.click('xpath/' + xpath);
+                            if (pilot) {
+                                for (let i = 0; i < indexToSelect; i++) {
+                                    //await console.log("Arrow");
+                                    await page.keyboard.press('ArrowDown');
+                                    await sleep(sleepms);
+                                }
+                                await page.keyboard.press('Enter');
+                                return;
+                            }
+                            for (let i = 0; i < indexToSelect; i++) {
+                                //await console.log("Arrow&enter" + indexToSelect);
+                                await page.keyboard.press('ArrowDown');
+                                await sleep(sleepms);
+                                await page.keyboard.press('Enter');
+                            }
+                        } catch (error) {
+                            console.error(`Seçim sırasında hata: ${error.message} Index: ${indexToSelect}`);
+                        }
+                    }
+
+                    async function moveMouse(page, xpath) {
+                        try {
+                            const [element] = await page.$$(xpath);
+                            if (!element) {
+                                console.log('XPath öğesi bulunamadı!');
+                                return;
+                            }
+
+                            const box = await element.boundingBox();
+                            if (!box) {
+                                console.log('Öğe boyutu alınamadı!');
+                                return;
+                            }
+
+                            const startX = box.x;
+                            const endX = box.x + box.width;
+                            const y = box.y + box.height / 2;
+
+                            // Sol tık basılı tutarak sağa doğru hareket etme
+                            //await page.mouse.move(startX, y);
+
+                            await page.mouse.down(); // Sol tık basılı tut
+                            for (let x = startX; x <= endX; x += 1) {
+                                await page.mouse.move(x, y);
+                                await sleep(sleepms / 100);
+                            }
+                            for (let x = endX; x >= startX; x -= 0.5) {
+                                await page.mouse.move(x, y);
+                                await sleep(sleepms / 200);
+                            }
+
+                            //await page.mouse.up(); // Sol tıkı bırak
+
+                        } catch (error) {
+                            console.error(`Mouse Move sırasında hata: ${error.message}`);
+                        }
+                    }
+
+                    await sleep(sleepms);
+                    await selectOption(page, "html/body/div/div[1]/body/div/div/div[1]/div/div[1]/div/div", _i, true);
+                    await sleep(sleepms);
+                    await selectOption(page, "html/body/div/div[1]/body/div/div/div[1]/div/div[2]/div/div", _j, true);
+                    await sleep(sleepms);
+                    await selectOption(page, 'html/body/div/div[1]/body/div/div/div[1]/div/div[3]/div/div/div[1]/div', _k, true);
+                    await sleep(sleepms);
+                    await selectOption(page, "/html/body/div/div[1]/body/div/div/div[1]/div/div[4]/div/div/div[1]/div", _p, true);
+                    await sleep(sleepms * 5);
+
+                    await page.evaluate(scriptContent);
+
+                    await moveMouse(page, "xpath/html/body/div/div[1]/body/div/div/div[2]/div/div[1]");
+                    await sleep(sleepms);
+                    await page.evaluate("console.log('Done For " + _i + "," + _j + "," + _k + "," + _p + "');");
+                    await page.close();
                 }
-                await page.keyboard.press('Enter');
-                return;
             }
-            for (let i = 0; i < indexToSelect; i++) {
-                await console.log("Arrow&enter" + indexToSelect);
-                await page.keyboard.press('ArrowDown');
-                await sleep(200);
-                await page.keyboard.press('Enter');
-            }
-        } catch (error) {
-            console.error(`Seçim sırasında hata: ${error.message} Index: ${indexToSelect}`);
         }
     }
-
-    // Yıl seçimi
-    await sleep(200);
-    await selectOption(page, "html/body/div/div[1]/body/div/div/div[1]/div/div[1]/div/div", 0,true);//2.indis seçildi yılda
-
-    // Pist seçimi
-    await sleep(200);
-    await selectOption(page, "html/body/div/div[1]/body/div/div/div[1]/div/div[2]/div/div", 2,true);//2.indis seçildi pistte
-
-    // Session seçimi
-    await sleep(200);
-    await selectOption(page, 'html/body/div/div[1]/body/div/div/div[1]/div/div[3]/div/div/div[1]/div', 2,true);//0.indis seçildi sessionda
-    // Pilot seçimi
-    await sleep(200);
-    await selectOption(page, "/html/body/div/div[1]/body/div/div/div[1]/div/div[4]/div/div/div[1]/div", 20, false);//7 pilot seçildi
-
-
     await console.log("Done!");
-    // await browser.close();
+    /*
+    YIllar:7
+    Pistler:22
+    Pilot:20
+    Şimdi şöyle
+    birden fazla pilot ekleyince arada veri kaçıyor o yüzden 1er 1er yaptım
+    ilk pilotu ekleyip sonra silip sonra ikincisini eklettirmiyor çünkü oradaki çarpı butonlarına bastırmıyor allah kahretsinki
+    full xpath vermeyince bulamıyor çünkü aynı class çok nesnede var ve class isimleri dinamik olabiliyor
+    pilotları toplu çekebiliyorum ama tek tek çekince daha sağlam oluyor.
+    sleepms azaltılırsa daha hızlı çalışır fakat min 100 olsun bence(internet hızına göre değişebiliyor)
+    aynı zamanda site arkada curl atıyor daha detaylı datalara erişebiliyoruz ama web scrapping ile bu şekilde oluyor geliştirilebilir tabii
+    */
 })();
